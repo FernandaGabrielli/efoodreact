c434643376e9406a29961d53c4d1f4411db14e85
Fernanda Gabrielli <123499117+FernandaGabrielli@users.noreply.github.com>
2024-06-23 21:41:24 -0300

    :lipstick: feat: Corrigindo cor de texto

--- src/components/ProfileHeader/styles.ts
+++ src/components/ProfileHeader/styles.ts
@@ -1,12 +1,11 @@
 import styled from 'styled-components'
-
 import bannerImg from '../../assets/images/Vector.png'
 import { breakpoints } from '../../styles'
 
 export const HeaderContainer = styled.div`
   display: flex;
   background-image: url(${bannerImg});
-  background-size: cotain;
+  background-size: contain;
 
   .container {
     height: 186px;
@@ -16,6 +15,15 @@ export const HeaderContainer = styled.div`
     text-decoration: none;
     font-size: 18px;
     font-weight: bold;
+
+    a {
+      color: inherit; /* Herda a cor do elemento pai */
+      text-decoration: none; /* Remove a decoração do texto */
+    }
+
+    a:visited {
+      color: inherit; /* Mantém a cor após o link ser visitado */
+    }
 
     @media (max-width: ${breakpoints.tablet}) {
       p {
